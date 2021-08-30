6074aa83d2b244b1261800a80b03236727e314e9
Tomi <tomasmazzocchi@est.frba.utn.edu.ar>
2021-08-30 16:40:58 -0300

    process finish

--- app.js
+++ app.js
@@ -1,22 +1,40 @@
 import { fillForm } from "./pdfLib.js";
 import XLSX from "xlsx";
 import fs from "fs";
+import capitalize from "capitalize";
 
 function leerDatosDesdeExcel(ruta, evento) {
   var workbook = XLSX.readFile(ruta);
   var workbookSheets = workbook.SheetNames;
 
-  workbookSheets.map(function (sheet) {
+  workbookSheets.forEach(function (sheet) {
     //Chequeo que exista la carpeta /Certificados/NombreSheet y si no existe la creo
+    console.log("Creando pdf's de hoja " + sheet);
     var dir = "./Certificados/" + sheet;
     if (!fs.existsSync(dir)) {
       fs.mkdirSync(dir);
     }
     //Convierto la hoja en json y se ejecuta el llenado y guardado de pdf
     var data = XLSX.utils.sheet_to_json(workbook.Sheets[sheet]);
-    data.map(function (record) {
-      fillForm(record.Nombre + " " + record.Apellido, evento, sheet);
+
+    data.forEach(function (record) {
+      try {
+        var nombreCompleto = capitalize.words(record.NombreApellido);
+        fillForm(nombreCompleto, evento).then((bytes) => {
+          //Guardo PDF
+          fs.writeFileSync(
+            "./Certificados/" +
+              sheet +
+              "/" +
+              nombreCompleto +
+              " - Certificado.pdf",
+            bytes
+          );
+        });
+      } catch (e) {
+        console.log(e);
+      }
     });
   });
 }
-leerDatosDesdeExcel("BD.xlsx", "carefaBsAs");
+leerDatosDesdeExcel("BD.xlsx", "Cleveland Clinic");
--- pdfLib.js
+++ pdfLib.js
@@ -2,25 +2,21 @@ import pdfLib from "pdf-lib";
 import fs from "fs";
 const { PDFDocument } = pdfLib;
 
-async function fillForm(nombreCompleto, evento, sheet) {
-  const formPdfBytes = fs.readFileSync(evento + ".pdf");
+async function fillForm(nombreCompleto, evento) {
+  var formPdfBytes = fs.readFileSync(evento + ".pdf");
 
-  const pdfDoc = await PDFDocument.load(formPdfBytes);
+  var pdfDoc = await PDFDocument.load(formPdfBytes);
 
-  const form = pdfDoc.getForm();
+  var form = pdfDoc.getForm();
 
-  const nameField = form.getTextField("nombreApellido");
+  var nameField = form.getTextField("nombreApellido");
   nameField.setText(nombreCompleto);
 
   form.flatten();
 
-  const pdfBytes = await pdfDoc.save();
+  var pdfBytes = await pdfDoc.save();
 
-  //Guardo PDF
-  fs.writeFileSync(
-    "./Certificados/" + sheet + "/" + nombreCompleto + " - Certificado.pdf",
-    pdfBytes
-  );
+  return pdfBytes;
 }
 
 export { fillForm };
